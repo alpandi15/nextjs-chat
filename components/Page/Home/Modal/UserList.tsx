419f3f9191e7d8069ed4112394fe4c48f6bbba18
Muhammad Al-Pandi <m.alpandi57@gmail.com>
2021-06-05 14:58:40 +0700

    List friends

--- components/Page/Home/Modal/UserList.tsx
+++ components/Page/Home/Modal/UserList.tsx
@@ -18,7 +18,8 @@ const UserList = () => {
           usersData?.map((val: any, index: number) => (
             <div className="flex mt-4 items-center bg-gray-300 p-2 rounded-lg" key={index}>
               <img src="/profile.png" className="w-12 h-12 object-cover rounded-full inline-block" alt="" />
-              <h3 className="text-xl pr-8 pl-2">{val?.friend?.username}</h3>
+              <h3 className="text-xl pr-8 pl-2">{val?.username}</h3>
+                <button onClick={() => console.log(val)} className="block p-1 text-sm bg-blue-400 text-white rounded-lg disabled:bg-gray-800">Tambah</button>
             </div>
           ))
         }
